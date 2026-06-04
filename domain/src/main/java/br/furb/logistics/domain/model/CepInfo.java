ec997e5f2dbb89fe192d5eb6e8b214774569e145
nktrapp <ntrapp@furb.br>
2026-06-04 13:32:02 -0300

    twentieth commit

--- domain/src/main/java/br/furb/logistics/domain/model/CepInfo.java
+++ domain/src/main/java/br/furb/logistics/domain/model/CepInfo.java
@@ -1,16 +1,24 @@
 package br.furb.logistics.domain.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.beans.ConstructorProperties;
+
 @Getter
-@Builder
-@AllArgsConstructor
 public class CepInfo {
 
     private final String cep;
     private final String city;
     private final String state;
     private final String neighborhood;
+
+    @Builder
+    @ConstructorProperties({"cep", "city", "state", "neighborhood"})
+    public CepInfo(String cep, String city, String state, String neighborhood) {
+        this.cep = cep;
+        this.city = city;
+        this.state = state;
+        this.neighborhood = neighborhood;
+    }
 }
